eeeb9a40a7c57855ad66a1119d36888e9ee9a1d6
Md Nazmul Islam <nazmulislampr3@gmail.com>
2024-10-26 14:26:08 +0600

    v10

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import cors from "cors";
 import express from "express";
 import errorHandler from "./middlewares/errorHandler.middleware";
 import routers from "./routes";
+import asyncHandler from "./utils/asyncHandler";
 
 const app = express();
 
@@ -11,6 +12,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1", routers);
+
+app.use(
+  "/",
+  asyncHandler(async (req, res) => {
+    return res.status(200).json({ testJson: true });
+  })
+);
 
 app.use(errorHandler as any);
 
