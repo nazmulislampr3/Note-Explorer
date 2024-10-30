8b27b99320deecd8dda4245d1544dca9f5a0838c
Md Nazmul Islam <nazmulislampr3@gmail.com>
2024-10-31 02:44:04 +0600

    v201

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 import express from "express";
 import errorHandler from "./middlewares/errorHandler.middleware";
-// import routers from "routes/index";
+import routers from "./routes";
 import asyncHandler from "./utils/asyncHandler";
 
 const app = express();
@@ -11,7 +11,7 @@ app.use(cors({ origin: process.env.CLIENT_URI }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use("/api/v1", routers);
+app.use("/api/v1", routers);
 
 app.use(
   "/",
