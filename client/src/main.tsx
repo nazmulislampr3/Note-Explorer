ef6a8afefc687919d9a1c8b1ed034d64452c95fc
Md Nazmul Islam <nazmulislampr3@gmail.com>
2024-10-31 01:52:21 +0600

    v100

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./root.scss";
 import "./index.scss";
 import UIContextProvider from "./context/UIContext.tsx";
 
